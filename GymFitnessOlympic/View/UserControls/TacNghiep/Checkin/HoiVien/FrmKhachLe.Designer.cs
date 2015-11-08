883d66a5dcb1b1d9bafbb9ed9a27f71886ce2926
hynguyen2610 <hynguyen2610@gmail.com>
2015-11-08 07:39:09 +0700

    ReUp with EF

---
+++ GymFitnessOlympic/View/UserControls/TacNghiep/Checkin/HoiVien/FrmKhachLe.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace GymFitnessOlympic.View.UserControls.TacNghiep.Checkin.HoiVien
+{
+    partial class FrmKhachLe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTien = new DevExpress.XtraEditors.TextEdit();
+            this.btnNhap = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.dxErrorProvider1 = new DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.txtTien.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dxErrorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số tiền";
+            // 
+            // txtTien
+            // 
+            this.txtTien.Location = new System.Drawing.Point(91, 12);
+            this.txtTien.Name = "txtTien";
+            this.txtTien.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTien.Properties.Appearance.Options.UseFont = true;
+            this.txtTien.Size = new System.Drawing.Size(348, 40);
+            this.txtTien.TabIndex = 1;
+            // 
+            // btnNhap
+            // 
+            this.btnNhap.Location = new System.Drawing.Point(150, 78);
+            this.btnNhap.Name = "btnNhap";
+            this.btnNhap.Size = new System.Drawing.Size(150, 44);
+            this.btnNhap.TabIndex = 2;
+            this.btnNhap.Text = "Nhập";
+            this.btnNhap.Click += new System.EventHandler(this.btnNhap_Click);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(306, 78);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(150, 44);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "Hủy";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // dxErrorProvider1
+            // 
+            this.dxErrorProvider1.ContainerControl = this;
+            // 
+            // FrmKhachLe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 163);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.btnNhap);
+            this.Controls.Add(this.txtTien);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmKhachLe";
+            this.Text = "Khách lẻ";
+            ((System.ComponentModel.ISupportInitialize)(this.txtTien.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dxErrorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.TextEdit txtTien;
+        private DevExpress.XtraEditors.SimpleButton btnNhap;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider dxErrorProvider1;
+    }
+}
---
+++ GymFitnessOlympic/View/UserControls/TacNghiep/Checkin/HoiVien/FrmKhachLe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GymFitnessOlympic.View.UserControls.TacNghiep.Checkin.HoiVien
+{
+    public partial class FrmKhachLe : Form
+    {
+        public FrmKhachLe()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            int tien;
+            var st = txtTien.Text;
+            if (!int.TryParse(st, out tien) || tien <= 0 || tien % 500 != 0) {
+                dxErrorProvider1.SetError(txtTien, "Tiền nhập vào không hợp lệ");
+                txtTien.Focus();
+                return;
+            }
+
+        }
+    }
+}
